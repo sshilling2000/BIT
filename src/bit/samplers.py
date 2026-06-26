b2df7edcc1d9b493c269a5f4dba3a672935075a6
Cairnsty <cmt96@hotmail.co.uk>
2026-06-26 11:23:58 +0100

    tidied samplers.py

--- src/bit/samplers.py
+++ src/bit/samplers.py
@@ -7,27 +7,35 @@ import corner
 #basic model - to be replaced with more complex models (or user-specified models)
 def linear_model(theta, x):
     m, b = theta
+
     return m * x + b
+
 
 #log prior prob.
 def log_prior(theta):
     m, b = theta
     if -10 < m < 10 and -10 < b < 10:
         return 0.0
+    
     return -np.inf
+
 
 #log prob
 def log_likelihood(theta, x, y, yerr):
     model = linear_model(theta, x)
     sigma2 = yerr**2
+
     return -0.5 * np.sum((y - model)**2 / sigma2)
+
 
 #posterior prob
 def log_prob(theta, x, y, yerr):
     lp = log_prior(theta)
     if not np.isfinite(lp):
         return -np.inf
+    
     return lp + log_likelihood(theta, x, y, yerr)
+
 
 #running the sampler
 def run_sampler(log_prob_fn, initial_guess, n_steps=2000, n_walkers=32, args=()):
@@ -58,6 +66,7 @@ def run_sampler(log_prob_fn, initial_guess, n_steps=2000, n_walkers=32, args=())
 
     return sampler
 
+
 #wrap the everything and run the sampler - this is our function that we provide for users
 def fit(log_prob_fn, initial_guess, args=(), burn_in=500):
     """
@@ -69,4 +78,5 @@ def fit(log_prob_fn, initial_guess, args=(), burn_in=500):
     sampler = run_sampler(log_prob_fn, initial_guess, args=args)
 
     samples = sampler.get_chain(discard=burn_in, flat=True)
+    
     return samples
